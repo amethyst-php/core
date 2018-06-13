9325605c804c2c6c84acf30b0255b147434a89ff
railken <railken@protonmail.ch>
2018-06-14 01:51:21 +0200

    Revert "add signIn"
    This reverts commit 169b7a03797485d607d62b212095bf4a77527939.

--- src/Testing/ApiTestableTrait.php
+++ src/Testing/ApiTestableTrait.php
@@ -2,8 +2,6 @@
 
 namespace Railken\LaraOre\Support\Testing;
 
-use Illuminate\Support\Facades\Config;
-
 trait ApiTestableTrait
 {
     public function commonTest($url, $parameters, $check = null)
@@ -49,19 +47,5 @@ trait ApiTestableTrait
         }
 
         $response->assertStatus($code);
-    }
-
-    public function signIn()
-    {
-        $response = $this->post(Config::get('ore.api.router.prefix').'/sign-in', [
-            'username' => 'admin@admin.com',
-            'password' => 'vercingetorige',
-        ]);
-
-        $access_token = json_decode($response->getContent())->data->access_token;
-
-        $this->withHeaders(['Authorization' => 'Bearer '.$access_token]);
-
-        return $response;
     }
 }
