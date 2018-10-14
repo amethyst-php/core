772c8a9241cd154ed247974e5449d38d64bdaf4c
railken <railken@protonmail.ch>
2018-10-14 17:14:47 +0200

    add ConfigurableManager

---
+++ src/ConfigurableManager.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Railken\Amethyst\Common;
+
+use Illuminate\Support\Facades\Config;
+
+trait ConfigurableManager
+{
+    /**
+     * Initialize the model by the configuration.
+     */
+    public function bootConfig()
+    {
+        $this->comment = Config::get($this->config.'.comment');
+    }
+
+    /**
+     * Register Classes.
+     */
+    public function registerClasses()
+    {
+        return Config::get($this->config);
+    }
+}
