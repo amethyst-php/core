5b4885693f08076a787b1d7a167b57256f252a0c
railken <railken@protonmail.ch>
2018-12-20 15:57:55 +0100

    fix

--- src/CommonServiceProvider.php
+++ src/CommonServiceProvider.php
@@ -69,7 +69,7 @@ class CommonServiceProvider extends ServiceProvider
         $inflector = new Inflector();
 
         $reflection = new \ReflectionClass($this);
-        $packageName = $inflector->tableize(str_replace("ServiceProvider", "", $reflection->getShortName()));
+        $packageName = str_replace("_", "-", $inflector->tableize(str_replace("ServiceProvider", "", $reflection->getShortName())));
         
         foreach (Config::get('amethyst.'.$packageName.'.http') as $groupName => $group) {
             foreach ($group as $configName => $config) {
