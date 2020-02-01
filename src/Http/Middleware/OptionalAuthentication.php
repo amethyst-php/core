e05362b89215c10a05d16ae80dbf934af40675c6
Railken <railken@protonmail.ch>
2020-02-01 17:39:03 +0100

    add optional authentication

---
+++ src/Http/Middleware/OptionalAuthentication.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Amethyst\Core\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Contracts\Auth\Factory as Auth;
+
+class OptionalAuthentication extends Authenticate
+{
+    public function handle($request, Closure $next, ...$guards)
+    {
+        try {
+            $this->authenticate($request, $guards);
+        } catch(AuthenticationException $e) {
+            // dont do anything
+        }
+
+        return $next($request);
+    }
+}
