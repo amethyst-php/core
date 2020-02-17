1275894ae1ea4bfa3ca0b535ff328182d2dda8c7
Railken <railken@protonmail.ch>
2020-02-17 23:05:55 +0100

    move boot time

--- src/Providers/AmethystServiceProvider.php
+++ src/Providers/AmethystServiceProvider.php
@@ -20,12 +20,11 @@ class AmethystServiceProvider extends ServiceProvider
         $this->app->singleton('amethyst', function ($app) {
             return new \Amethyst\Core\Helper();
         });
+        app('amethyst')->boot();
     }
 
     public function boot()
     {
-        app('amethyst')->boot();
-        
         $this->loadRoutesFrom(__DIR__.'/../../resources/routes.php');
     }
 }
