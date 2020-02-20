287654e93f6efbe3326ee4363bf442c65f95ed90
Railken <railken@protonmail.ch>
2020-02-20 08:52:08 +0100

    base map key-mapping

--- src/Map.php
+++ src/Map.php
@@ -3,6 +3,7 @@
 namespace Amethyst\Core;
 
 use Railken\EloquentMapper\Map as BaseMap;
+use Illuminate\Database\Eloquent\Model;
 
 class Map extends BaseMap
 {
@@ -14,7 +15,32 @@ class Map extends BaseMap
     public function models(): array
     {
         return app('amethyst')->getData()->map(function ($data) {
+            $data->boot();
             return $data->newEntity();
         })->all();
+    }
+
+    /**
+     * Convert a model to a unique key
+     *
+     * @param Model $model
+     *
+     * @return string
+     */
+    public function modelToKey(Model $model): string
+    {
+        return $model->getMorphClass();
+    }
+
+    /**
+     * Convert key to a new instance of a model
+     *
+     * @param string $key
+     *
+     * @return Model
+     */
+    public function keyToModel(string $key): Model
+    {
+        return app('amethyst')->findManagerByName($key)->newEntity();
     }
 }
